173dd7639db4b63d3b2d257e5024284e383e57d5
Rene Kjellerup <rk.katana.steel@gmail.com>
2021-09-20 10:19:18 -0700

    added rmode stub functions to the openGL drivers

--- runtime/render_a/src/sys/ogl/common_init.cpp
+++ runtime/render_a/src/sys/ogl/common_init.cpp
@@ -7,3 +7,19 @@ void rdll_OGlRenderSetup(RenderStruct *pStruct)
 {
 
 }
+
+RMode* rdll_GetSupportedModes() // RKJ STUB for the openGL Renderer
+{
+    return nullptr;
+}
+
+void rdll_FreeModeList(RMode *pCur) // free the allocated RMode linked list
+{
+    RMode *pNext = nullptr;
+    while (pCur)
+    {
+        pNext = pCur->m_pNext;
+        delete pCur;
+        pCur = pNext;
+    }
+}
